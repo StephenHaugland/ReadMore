24be49d6e23751b2c27bd707147fbea59d2da104
StephenHaugland <43276864+StephenHaugland@users.noreply.github.com>
2024-10-16 11:40:54 -0700

    SameSite cookie attribute

--- app.js
+++ app.js
@@ -75,6 +75,7 @@ const sessionConfig = {
     proxy: true,
     cookie: {
         httpOnly: true,
+        sameSite: 'None',
         secure: true,
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7
